be83e65b7b083d14a73ec760bf5f3dedb8c5a6d7
ridanaeeem <ridan@bu.edu>
2024-09-22 16:15:31 -0400

    12 icon + resize + change_icon logic update

--- change_icon.sh
+++ change_icon.sh
@@ -1,35 +1,7 @@
 #!/bin/bash
 
 current_hour=$(date +"%H")  #00-23
-current_minute=$(date +"%M")  #00-59
-
-if [ $current_hour -ge 0 ] && [ $current_hour -lt 2 ]; then
-    subdirectory="1"
-elif [ $current_hour -ge 2 ] && [ $current_hour -lt 4 ]; then
-    subdirectory="2"
-elif [ $current_hour -ge 4 ] && [ $current_hour -lt 6 ]; then
-    subdirectory="3"
-elif [ $current_hour -ge 6 ] && [ $current_hour -lt 8 ]; then
-    subdirectory="4"
-elif [ $current_hour -ge 8 ] && [ $current_hour -lt 10 ]; then
-    subdirectory="5"
-elif [ $current_hour -ge 10 ] && [ $current_hour -lt 12 ]; then
-    subdirectory="1"
-elif [ $current_hour -ge 12 ] && [ $current_hour -lt 14 ]; then
-    subdirectory="1"
-elif [ $current_hour -ge 14 ] && [ $current_hour -lt 16 ]; then
-    subdirectory="2"
-elif [ $current_hour -ge 16 ] && [ $current_hour -lt 18 ]; then
-    subdirectory="3"
-elif [ $current_hour -ge 18 ] && [ $current_hour -lt 20 ]; then
-    subdirectory="4"
-elif [ $current_hour -ge 20 ] && [ $current_hour -lt 22 ]; then
-    subdirectory="5"
-elif [ $current_hour -ge 22 ] && [ $current_hour -lt 24 ]; then
-    subdirectory="1"
-else
-    subdirectory="1"
-fi
+subdirectory=$(((current_hour % 12) + 1 ))
 
 /opt/homebrew/bin/fileicon set /Users/rida/Desktop/docs /Users/rida/repos/autoBG/icons/$subdirectory/1.png
 /opt/homebrew/bin/fileicon set /Users/rida/Desktop/photo /Users/rida/repos/autoBG/icons/$subdirectory/2.png
